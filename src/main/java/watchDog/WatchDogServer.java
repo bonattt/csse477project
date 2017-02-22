6f6a496cfa35f02f55349a4fa9ac93722e8d7920
bonattt <bonattt@rose-hulman.edu>
2017-02-22 18:37:25 -0500

    watchdog tests complete, no errors, 5 failures

--- src/main/java/watchDog/WatchDogServer.java
+++ src/main/java/watchDog/WatchDogServer.java
@@ -3,37 +3,139 @@ package watchDog;
 import iServer.IServer;
 import iServer.IServlet;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 
 public class WatchDogServer implements Runnable {
 
 	private static final int WAIT_TIME = 100;
 	private static final int DEFAULT_TICKS = 100;
 
-	private int count;
-	private int maxCount; 
+	private int count, maxCount; 
+	private String notifiedQueue, watchedQueue;
 	private boolean keepRunning;
+
+	private Channel notifiedChannel, watchedChannel;
+	private Connection notifiedConnection, watchedConnection;
+	
+	private String message;
+	private boolean messageReceived;
+	
 	private Logger logger = LogManager.getLogger(this.getClass());
 	
-	public WatchDogServer() {
-		this(WAIT_TIME * DEFAULT_TICKS);
+	public WatchDogServer(String notifiedQueue, String watchedQueue)
+				throws IOException, TimeoutException {
+		this(notifiedQueue, watchedQueue, WAIT_TIME * DEFAULT_TICKS);
 	}
 	
-	protected WatchDogServer(int milis) {
+	protected WatchDogServer(String notifyQueue, String checkQueue, int milis) 
+				throws IOException, TimeoutException {
+		this.notifiedQueue = notifyQueue;
+		this.watchedQueue = checkQueue;
 		keepRunning = true;
 		setTimeLimit(milis);
+		setupWatchedQueue();
+		try {
+			setupNotifiedQueue();
+		} catch (IOException e) {
+			logger.error("IOExcetpion: " + e.getMessage());
+//			throw e;
+		}
+	}
+	
+	private void setupWatchedQueue() throws IOException, TimeoutException {
+		logger.info("setting up watched queue + '" + watchedQueue + "'");
+		try {
+			ConnectionFactory factory = new ConnectionFactory();
+		    factory.setHost("localhost");
+		    watchedConnection = factory.newConnection();
+		    watchedChannel = watchedConnection.createChannel();
+//		    watchedChannel.queueDeclare(watchedQueue, false, false, false, null);
+		    logger.info("successfully declared queue '" + watchedQueue + "'");
+		} catch (IOException e) {
+			logger.info("IOException: " + e.getMessage());
+		}
+	}
+	
+	private void setupNotifiedQueue() throws IOException, TimeoutException {
+		logger.info("setting up notified queue: '" + notifiedQueue + "'");
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    notifiedConnection = factory.newConnection();
+	    notifiedChannel = notifiedConnection.createChannel();
+
+//	    notifiedChannel.queueDeclare(notifiedQueue, false, false, false, null);
+
+	    Consumer consumer = new DefaultConsumer(notifiedChannel) {
+	      @Override
+	      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+	          throws IOException {
+	    	logger.info("received a message!");
+	        message = new String(body, "UTF-8");
+	        logger.info(String.format("received message '%s'", message));
+	        reset();
+	        logger.info("finished handling message '" + message + "'");
+	      }
+	    };
+	    notifiedChannel.basicConsume(notifiedQueue, true, consumer);
+		logger.info("notified queue: '" + notifiedQueue + "' is now consuming messages.");
 	}
 	
 	@Override
 	public void run() {
 		logger.info("starting WatchDogServer");
 		while(keepRunning) {
+			checkForMessages();
+			if (isTimedOut()) {
+				logger.warn("watch dog timed out!");
+				notifyApp();
+			}
+			try {
+				Thread.sleep(WAIT_TIME);
+			} catch (InterruptedException e) { /* do nothing */ }
 			tick();
 		}
 		logger.info("WatchDogServer finished");
+
+		try {
+			notifiedChannel.close();
+		} catch (Exception e) {
+			logger.warn("Exception thrown closing notifiedChannel");
+			logger.info(e + " : msg = " + e.getMessage());
+		}
+
+		try {
+			watchedChannel.close();
+		} catch (Exception e) {
+			logger.warn("Exception thrown closing watchedChannel");
+			logger.info(e + " : msg = " + e.getMessage());
+		}
+
+		try {
+			notifiedConnection.close();
+		} catch (Exception e) {
+			logger.warn("Exception thrown closing notifiedConnection");
+			logger.info(e + " : msg = " + e.getMessage());
+		}
+
+		try {
+			watchedConnection.close();
+		} catch (Exception e) {
+			logger.warn("Exception thrown closing watchedConnection");
+			logger.info(e + " : msg = " + e.getMessage());
+		}
 	}
 	
 	public boolean isTimedOut() {
@@ -52,6 +154,24 @@ public class WatchDogServer implements Runnable {
 		if (milis % WAIT_TIME != 0) {
 			maxCount =+ 1;
 		}
+	}
+	
+	public synchronized void stop() {
+		logger.info("stopping watch dog server!");
+		if (!keepRunning) {
+			logger.warn("Watch Dog Server already stopped!");
+		}
+		keepRunning = false;
+	}
+	
+	public void notifyApp() {
+		logger.info("notifying queue " + notifiedQueue);
+		
+	}
+	
+	public boolean checkForMessages() {
+		logger.info("checking for messages from " + watchedQueue);
+		return false;
 	}
 	
 	public synchronized void tick() {
--- src/test/java/watchDog/TestWatchDogServer.java
+++ src/test/java/watchDog/TestWatchDogServer.java
@@ -2,18 +2,106 @@ package watchDog;
 
 import static org.junit.Assert.*;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
+import java.util.concurrent.TimeoutException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.AlreadyClosedException;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 
 public class TestWatchDogServer {
 
+	private static String watchedQueue = "Watched Queue";
+	private static String notifiedQueue = "Notified Queue";
+	private static final String DEFAULT_MSG = "nothing received";
+	
+	private static Channel notifiedChannel, watchedChannel;
+	private static Connection notifiedConnection, watchedConnection;
+
 	private WatchDogServer server;
+	private static String message;
+	private static boolean messageReceived;
+	
+	private static Logger logger = LogManager.getLogger(TestWatchDogServer.class);
 	
 	@Before
-	public void setup() {
-		server = new WatchDogServer();
+	public void setup() throws IOException, TimeoutException {
+		server = new WatchDogServer(notifiedQueue, watchedQueue);
+		message = DEFAULT_MSG;
+		messageReceived = false;
+		setupWatchedQueue();
+		setupNotifiedQueue();
+		
+	}
+	
+	@After
+	public void tearDown() {
+		server.stop();
+	}
+	
+//	@AfterClass
+//	public void tearDown() throws IOException, TimeoutException {
+//		try {
+//			notifiedChannel.close();
+//		} catch (AlreadyClosedException e) {/* do nothing */}
+//		try {
+//			watchedChannel.close();
+//		} catch (AlreadyClosedException e) {/* do nothing */}
+//		try {
+//			notifiedConnection.close();
+//		} catch (AlreadyClosedException e) {/* do nothing */}
+//		try {
+//			watchedConnection.close();
+//		} catch (AlreadyClosedException e) {/* do nothing */}
+//	}
+	
+	private static void setupWatchedQueue() throws IOException, TimeoutException {
+		try {
+			ConnectionFactory factory = new ConnectionFactory();
+		    factory.setHost("localhost");
+		    watchedConnection = factory.newConnection();
+		    watchedChannel = watchedConnection.createChannel();
+//		    watchedChannel.queueDeclare(watchedQueue, false, false, false, null);
+		    watchedQueue = watchedChannel.queueDeclare().getQueue();
+		} catch (IOException e) {
+			logger.info("IOException: " + e.getMessage());
+			logger.error(e);
+		}
+	}
+	
+	private static void setupNotifiedQueue() throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    notifiedConnection = factory.newConnection();
+	    notifiedChannel = notifiedConnection.createChannel();
+//	    notifiedChannel.queueDeclare(notifiedQueue, false, false, false, null);
+	    notifiedQueue = notifiedChannel.queueDeclare().getQueue();
+
+	    Consumer consumer = new DefaultConsumer(notifiedChannel) {
+	      @Override
+	      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+	          throws IOException {
+	        message = new String(body, "UTF-8");
+	        messageReceived = true;
+	      }
+	    };
+	    notifiedChannel.basicConsume(notifiedQueue, true, consumer);
 	}
 	
 	@Test
@@ -65,7 +153,7 @@ public class TestWatchDogServer {
 		
 		assertTrue(server.isTimedOut());
 	}
-	
+
 	@Test
 	public void testIsNotTimedOut() 
 				throws NoSuchFieldException, SecurityException,
@@ -75,6 +163,77 @@ public class TestWatchDogServer {
 		f.setInt(server, 0);
 		
 		assertFalse(server.isTimedOut());
+	}
+
+	@Test(timeout=1000)
+	public void testWatchDogStopsWhenStopped() 
+				throws NoSuchFieldException, SecurityException,
+					IllegalArgumentException, IllegalAccessException {
+		Thread t = new Thread(server);
+		t.start();
+		server.stop();
+	}
+
+	@Test
+	public void testCheckForMessage() 
+				throws NoSuchFieldException, SecurityException,
+					IllegalArgumentException, IllegalAccessException {
+		Field f = WatchDogServer.class.getDeclaredField("count");
+		f.setAccessible(true);
+		f.setInt(server, 0);
+		
+		assertFalse(server.isTimedOut());
+	}
+
+	@Test(timeout=2000)
+	public void testReceivesNewMessage() throws UnsupportedEncodingException, IOException {
+		String sent = "hello";
+		watchedChannel.basicPublish("", watchedQueue, null, sent.getBytes("UTF-8"));
+		assertTrue(server.checkForMessages());
+	}
+
+	@Test(timeout=2000)
+	public void testReceivesCorrectMessage() throws UnsupportedEncodingException, IOException {
+		fail("unimplemented");
+		String sent = "hello";
+		watchedChannel.basicPublish("", watchedQueue, null, sent.getBytes("UTF-8"));
+	}
+	
+	@Test(timeout=2000)
+	public void testNotifyAppSendsMsg()  {
+		server.notifyApp();
+		while(! messageReceived) {
+			// busy wait.
+		}
+		assertNotEquals(DEFAULT_MSG, message);
+	}
+
+	@Test(timeout=2000)
+	public void testNotifyAppSendsCorrectMsg()  {
+		String expected = "";
+		server.notifyApp();
+		while(! messageReceived) {
+			// busy wait.
+		}
+		assertEquals(expected, message);
+	}
+
+	@Test(timeout=2000)
+	public void testNoNewMessages() {
+		assertFalse(server.checkForMessages());
+	}
+
+	@Test(timeout=2000)
+	public void testReceivingMessageResetsCount()
+				throws NoSuchFieldException, SecurityException, 
+					IllegalArgumentException, IllegalAccessException, 
+					UnsupportedEncodingException, IOException {
+		Field f = WatchDogServer.class.getDeclaredField("count");
+		f.setAccessible(true);
+		f.setInt(server, 10);
+		
+	    watchedChannel.basicPublish("", watchedQueue, null, message.getBytes("UTF-8"));
+	    assertEquals(0, f.getInt(server));
 	}
 
 }
